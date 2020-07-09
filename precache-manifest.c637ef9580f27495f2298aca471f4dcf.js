747f55531d538a5ff7ff96eaa6e64029b433fe59
B0218955 <b0218950@LTB0218955-Mac.local>
2020-07-09 13:18:42 +0530

    Updates

---
+++ precache-manifest.c637ef9580f27495f2298aca471f4dcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2ed0757cd108c0b5997f2c070cea4e1",
+    "url": "/temp-app/index.html"
+  },
+  {
+    "revision": "0b3841963a2db0ed6930",
+    "url": "/temp-app/static/css/main.d9529271.chunk.css"
+  },
+  {
+    "revision": "521c8a1907f14f6ecf91",
+    "url": "/temp-app/static/js/2.1e45bb6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/temp-app/static/js/2.1e45bb6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b3841963a2db0ed6930",
+    "url": "/temp-app/static/js/main.6874df01.chunk.js"
+  },
+  {
+    "revision": "b7c2bb2bf3d72e4387ee",
+    "url": "/temp-app/static/js/runtime-main.1b16a2d3.js"
+  }
+]);
